baece5f8c3ba9a7b552d54f1955340c6f055ac2b
jungooji <zzzz465@naver.com>
2024-06-04 21:06:36 +0900

    add IScore interface

--- VRarcher/Assets/components/ScoreManager.cs
+++ VRarcher/Assets/components/ScoreManager.cs
@@ -1,31 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static ScoreManager Instance { get; private set; }
-    private int totalScore = 0;
+	public static ScoreManager Instance { get; private set; }
+	private List<IScore> scores;
+	public int TotalScore
+	{
+		get
+		{
+			return this.scores.Select((x) => x.GetScore()).Sum();
+		}
+	}
 
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-    }
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
 
-    public void AddScore(int score)
-    {
-        totalScore += score;
-        Debug.Log("Total Score: " + totalScore);
-    }
+		this.scores = new List<IScore>();
+	}
 
-    public int GetScore()
-    {
-        return totalScore;
-    }
+	public void Reset()
+	{
+		this.scores.Clear();
+	}
+
+	public void AddScore(IScore score)
+	{
+		this.scores.Add(score);
+		Debug.Log("Total Score: " + this.TotalScore);
+	}
 }
